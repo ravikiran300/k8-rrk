9a1caaf050cde77d9c09921a883f250ef34f45b0
ravikiran300 <54069290+ravikiran300@users.noreply.github.com>
2021-09-01 19:33:48 +0530

    Update Nodes-common.sh

--- Nodes-common.sh
+++ Nodes-common.sh
@@ -1,11 +1,17 @@
 #! /bin/bash
 
 
+#The Kubernetes scheduler determines the best available node on which to deploy newly created pods.
+#If memory swapping is allowed to occur on a host system, this can lead to performance and stability issues within Kubernetes.
 
 # disable swap 
 sudo swapoff -a
+
 # keeps the swaf off during reboot
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+#=======================================================================================================
+#Update the apt package index and install packages to allow apt to use a repository over HTTPS
 
 sudo apt-get update -y
 
@@ -16,6 +22,7 @@ sudo apt-get update -y
     gnupg \
     lsb-release
 
+#Add Docker’s official GPG key:
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo \
@@ -24,6 +31,7 @@ echo \
 
 sudo apt-get update -y
 
+#Install docker pacakage
 sudo apt-get  install -y docker-ce docker-ce-cli containerd.io 
 
 
@@ -39,12 +47,31 @@ cat <<EOF | sudo tee /etc/docker/daemon.json
 }
 EOF
 
+#Letting iptables see bridged traffic
+sudo modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+sudo sysctl --system
+
 sudo systemctl enable docker
 sudo systemctl daemon-reload
 sudo systemctl restart docker
 
 echo "Docker Runtime Configured Successfully"
 
+#==================================================================================
+
+#kubeadm: the command to bootstrap the cluster.
+#kubelet: the component that runs on all of the machines in your cluster and does things like starting pods and containers.
+#kubectl: the command line util to talk to your cluster
 
 sudo apt-get update -y 
 sudo apt-get install -y apt-transport-https ca-certificates curl
@@ -55,8 +82,11 @@ echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https:/
 sudo apt-get update -y
 
 #sudo apt-get install -y kubelet=$KUBERNETES_VERSION kubectl=$KUBERNETES_VERSION kubeadm=$KUBERNETES_VERSION
+
+#install binaries
 sudo apt-get install -y kubelet kubeadm kubectl
 
+#apt-mark will mark or unmark a software package as being automatically installed and it is used with option hold
 sudo apt-mark hold kubelet kubeadm kubectl
 
 
