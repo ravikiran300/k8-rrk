d652e1c499b9fe7f4f8a4a2b584dea1877a821d3
ravikiran300 <54069290+ravikiran300@users.noreply.github.com>
2021-09-06 10:15:35 +0530

    Update Nodes-common.sh

--- Nodes-common.sh
+++ Nodes-common.sh
@@ -47,19 +47,6 @@ cat <<EOF | sudo tee /etc/docker/daemon.json
 }
 EOF
 
-#Letting iptables see bridged traffic
-sudo modprobe br_netfilter
-
-cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
-br_netfilter
-EOF
-
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-EOF
-
-sudo sysctl --system
 
 sudo systemctl enable docker
 sudo systemctl daemon-reload
